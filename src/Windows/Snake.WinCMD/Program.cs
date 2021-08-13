5503af4cd5d153e2d05c3529be2e20f3a07d5a3f
Vladimir Skopin <vl.skopin@gmail.com>
2021-08-14 00:34:21 +0700

    Добавьте файлы проекта.

---
+++ src/Windows/Snake.WinCMD/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WinCMD
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.SetWindowSize(80,25);
+            Console.SetBufferSize(80,25);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
