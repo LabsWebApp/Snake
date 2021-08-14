e373325c3dd22ad9963c4666a0576995f8ebe0f2
Vladimir Skopin <vl.skopin@gmail.com>
2021-08-14 10:31:03 +0700

    Рисуем точки в консоль.

---
+++ src/Shared/Snake.Library/Shapes/Point.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+namespace Library.Shapes
+{
+    public struct Point
+    {
+        public Point(int x, int y, char symbol)
+        {
+            Symbol = symbol;
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public char Symbol { get; set; }
+
+        public void Draw()
+        {
+            SetCursorPosition(X, Y);
+            Write(Symbol);
+        }
+    }
+}
--- src/Windows/Snake.WinCMD/Program.cs
+++ src/Windows/Snake.WinCMD/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static System.Console;
+using Library.Shapes;
 
 namespace WinCMD
 {
@@ -6,9 +8,28 @@ namespace WinCMD
     {
         static void Main()
         {
-            Console.SetWindowSize(80,25);
-            Console.SetBufferSize(80,25);
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
+            ReadKey();
+            Clear();
+            SetWindowSize(80,25);
+            SetBufferSize(80,25);
+            var p1 = new Point(5, 6, '1');
+            var p2 = new Point(10, 17, '2');
+
+            p1.Draw();
+            p2.Draw();
+
+            ReadKey();
+
+            var p3 = p1;
+
+            p3.Symbol = '3';
+            p1.X = 20;
+
+            p1.Draw();
+            p3.Draw();
+
+            ReadKey();
         }
     }
 }
